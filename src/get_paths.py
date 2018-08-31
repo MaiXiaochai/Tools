85058c819d83f2cf8fa005e56c944bb46c8caad6
maixiaochai <maixiaochai@qq.com>
2018-08-31 15:38:29 +0800

    +)get_paths.py

---
+++ src/get_paths.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+def get_paths(dir_path):
+    """
+    生成器。
+    遍历指定目录下的所有非目录文件。
+    :param dir_path: str/要遍历的文件
+    :return: str/文件路径
+    """
+
+    for path in os.listdir(dir_path):
+        tmp_path = dir_path + path
+
+        if os.path.isdir(tmp_path):
+                yield from get_paths(tmp_path + '/')
+
+        else:
+            yield tmp_path
+
+
+if __name__ == "__main__":
+    the_path = './'
+    for i in get_paths(the_path):
+        print(i)
