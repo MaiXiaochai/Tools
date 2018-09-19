172cacba9a0f3592a88ce098e02cde84b6d6df91
maixiaochai <maixiaochai@qq.com>
2018-09-19 21:34:45 +0800

    +)为get_paths.py添加扫描深度

--- src/get_paths.py
+++ src/get_paths.py
@@ -3,19 +3,27 @@
 import os
 
 
-def get_paths(dir_path):
+def get_paths(dir_path, depth=None):
     """
     生成器。
-    遍历指定目录下的所有非目录文件。
+    遍历指定目录下的所有非目录文件, 不会列出目录路径。
+    注意：这里的路径使用'/'。
     :param dir_path: str/要遍历的目录路径
-    :return: str/文件路径
+    :param depth: int/扫描的深度 0:当前目录，1：当前目录的下一级目录
+    :return: str/文件路径， 若当前深度下为发现文件，则不返回。
     """
+    depth_count = 0
+
+    depth = int(depth) if depth else 0
+    dir_path = dir_path if dir_path.endswith('/') else dir_path + '/'
 
     for path in os.listdir(dir_path):
         tmp_path = dir_path + path
 
         if os.path.isdir(tmp_path):
-            yield from get_paths(tmp_path + '/')
+            if depth_count < depth:
+                depth_count += 1
+                yield from get_paths(tmp_path + '/', depth - 1)
 
         else:
             yield tmp_path
@@ -23,5 +31,5 @@ def get_paths(dir_path):
 
 if __name__ == "__main__":
     the_path = './'
-    for i in get_paths(the_path):
+    for i in get_paths(the_path, 2):
         print(i)
