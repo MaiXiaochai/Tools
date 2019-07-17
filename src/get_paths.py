dba2271f7c573cb81ebbb7479244c551424603ff
MaiXiaochai <maixiaochai@qq.com>
2019-07-17 10:05:32 +0800

    *）修复一些bug，功能加强，可以指定只返回特定后缀的文件。 *）修改开头注释；

--- src/get_paths.py
+++ src/get_paths.py
@@ -1,35 +1,53 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
+# File:    : get_paths.py
+# @Date    : 2019-07-17 16:19:38
+# @Author  : MaiXiaochai
+# Email    : maixiaochai@outlook.com
+# GitHub   : https://github.com/MaiXiaochai
+# @GitPage : https://maixiaochai.github.io
+
+from os import listdir
+from os.path import isdir, splitext
 
 
-def get_paths(dir_path, depth=None):
+def list_paths(dir_path, depth=0, suffix=None):
     """
-    生成器。
-    遍历指定目录下的所有非目录文件, 不会列出目录路径。
-    注意：这里的路径使用'/'。
-    :param dir_path: str/要遍历的目录路径
-    :param depth: int/扫描的深度 0:当前目录，1：当前目录的下一级目录
-    :return: str/文件路径， 若当前深度下为发现文件，则不返回。
+    1) Generator。
+    2) 遍历 dir_path 目录下 所有后缀为 suffix 的文件的路径。
+    3) 注意：这里的路径使用'/'。
+    :param dir_path:    str     要遍历的目录路径
+    :param depth:       int     扫描的深度 0:当前目录，1：当前目录的下一级目录
+    :param suffix:      str     文件后缀，如 ".py" 或者 "py"
+    :return:            str     文件路径
     """
-    depth_count = 0
 
-    depth = int(depth) if depth else 0
-    dir_path = dir_path if dir_path.endswith('/') else dir_path + '/'
+    # 设定当前目录的表示值
+    current_dir_level = 0
 
-    for path in os.listdir(dir_path):
-        tmp_path = dir_path + path
+    dir_path = dir_path if dir_path.endswith("/") else dir_path + "/"
+    suffix = suffix if suffix.startswith('.') else '.' + suffix
 
-        if os.path.isdir(tmp_path):
-            if depth_count < depth:
-                depth_count += 1
-                yield from get_paths(tmp_path + '/', depth - 1)
+    for _path in listdir(dir_path):
+        tmp_path = dir_path + _path
 
-        else:
+        if isdir(tmp_path):
+            if current_dir_level < depth:
+                yield from list_paths(tmp_path, suffix, depth - 1)
+
+        elif splitext(tmp_path)[-1] == suffix:
             yield tmp_path
 
 
-if __name__ == "__main__":
-    the_path = './'
-    for i in get_paths(the_path, 2):
+def demo():
+    test_path = './'
+    depth = 1       # 当前目录和下一级目录
+    suffix = "py"   # 搜索后缀为"py"的文件
+    res = list_paths(test_path, depth, suffix)
+    for i in res:
         print(i)
+
+
+if __name__ == "__main__":
+    demo()
