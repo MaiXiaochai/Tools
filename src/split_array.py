dadcec23434b929b33eacf487ef523ff7c6563e7
MaiXiaochai <maixiaochai@qq.com>
2019-12-16 10:23:38 +0800

    *)修改一个参数的命名；修改注释使其更加易懂

--- src/split_array.py
+++ src/split_array.py
@@ -1,22 +1,34 @@
 # -*-coding: utf-8 -*-
 
 
-def nbr_clip(start, end, clip_long):
+def nbr_clip(start, end, clip_length):
     """
-    将数轴按照 clip_long 的长度切分为 n 个段，返回 左闭右开区间[x, y)，
-    最后一个片段的右边界为 end + 1。所以，最好当做 左闭右开区间处理使用，[x, y)
+    将步长为1的数轴按照 clip_length 的长度切分为 n 个段，返回 N个左闭右开区间[x, y)，
+    最后一个片段的右边界为 end + 1。所以，最好当做 左闭右开区间处理使用，[x, y)。
+
+    ----------------------------------[ DEMO ]----------------------------------
+    def demo():
+    ls = [1, 2, 3, 4, 5]
+    split_array = nbr_clip(1, 5, 2)
+    for i in split_array:
+        print(i)
+
+    # Out:
+    # (1, 3)
+    # (3, 6)
+    -----------------------------------[ End ]----------------------------------
     :param start:           int/数轴起点
     :param end:             int/数轴终点
-    :param clip_long:       int/切片长度
+    :param clip_length:     int/切片长度
     :return:                tuple/当前片段的开始、结束位置(start_int, end_start)
     """
     total_long = end - start
-    total_clip = total_long // clip_long + 1
+    total_clip = total_long // clip_length + 1
 
     _left = _right = start
 
     for i in range(total_clip):
-        _left, _right = _right, _right + clip_long
+        _left, _right = _right, _right + clip_length
         is_end = _right >= end
 
         if is_end:
@@ -33,6 +45,10 @@ def demo():
     split_array = nbr_clip(1, 5, 2)
     for i in split_array:
         print(i)
+
+    # Out:
+    # (1, 3)
+    # (3, 6)
 
 
 if __name__ == "__main__":
